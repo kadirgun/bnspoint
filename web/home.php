72a92dcdca5ce071d35fda72a0843bc5a4596144
Kadir Gün <kadir@kadirgun.com>
2017-07-20 01:18:02 +0300

    Add files via upload

--- web/home.php
+++ web/home.php
@@ -17,11 +17,11 @@
 	<div class="row">
 		<div class="col-md-12">
 		    <div class="well text-center">
-		        <form role="form" class="form number-form" id="point-form">
+		        <form role="form" class="form" id="point-form">
 		        	<div class="row">
 					  <div class="col-lg-12">
 					    <div class="input-group">
-					      <input type="text" id="username" name="username" class="form-control" placeholder="Karakter Adı">
+					      <input type="text" id="username" name="username" class="form-control" placeholder="Karakter Adı" autocomplete="off">
 					      <span class="input-group-btn">
 					        <button class="btn btn-primary" type="submit">Hesapla</button>
 					      </span>
@@ -31,16 +31,39 @@
 		        </form>
 		    </div>
 		</div>
+		<div class="col-md-12">
+			<div class="list-group" id="suggest-list">
+			</div>
+		</div>
 	</div>
 </div>	
 		<script type="text/javascript" src="/assets/plugins/jquery.min.js"></script>
 		<script type="text/javascript" src="/assets/plugins/bootstrap/js/bootstrap.min.js"></script>
-		<script type="text/javascript" src="/assets/js/main.js"></script>
 		<script type="text/javascript">
 			$("#point-form").submit(function(){
 				location = "/"+$("#username").val();
 				return false;
-			})
+			});
+
+			$("#username").on("keyup", function(){
+				$("#suggest-list").html("");
+				jQuery.ajax({
+					url:"http://eu-search.ncsoft.com/openapi/suggest.jsp",
+					dataType:"jsonp",
+					data:{
+						site:"bns",
+						display:10,
+						collection:"bnsusersuggest",
+						query:$("#username").val()
+					},
+					success:function(data){
+						for(i=0;i<data.front.length;i++){
+							var username = data.front[i][0];
+							$("suggest-list").append('<a href="/'+username+'">'+username+'</a>');
+						}
+					}
+				})
+			});
 		</script>
 </body>
 </html>
