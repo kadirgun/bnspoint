9f5431113004e8dcfa28c28222d62ca6db1de960
Kadir Gün <kadir@kadirgun.com>
2017-07-20 01:29:47 +0300

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,4 +1,8 @@
 <?php
+  if ($_SERVER['SERVER_PORT'] == 443) {
+  header("Location: http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
+  exit;
+  }
   if(!empty(@$_GET['username'])){
     require_once('calculate.php');
   }else{
