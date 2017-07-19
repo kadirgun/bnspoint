70209e9799a1b4eb2f002bf444b1ce08db06bf2c
Kadir Gün <kadir@kadirgun.com>
2017-07-20 01:21:06 +0300

    Add files via upload

--- web/calculate.php
+++ web/calculate.php
@@ -326,6 +326,5 @@
 	</div>	
 	<script type="text/javascript" src="/assets/plugins/jquery.min.js"></script>
 	<script type="text/javascript" src="/assets/plugins/bootstrap/js/bootstrap.min.js"></script>
-	<script type="text/javascript" src="/assets/js/main.js"></script>
 </body>
 </html>
